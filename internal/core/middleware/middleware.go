f622a129fb772d214ebe4895b47e49b761f6f6a9
iexcalibur <shubhamkannojia10@gmail.com>
2024-12-09 06:22:05 +0530

    fix

--- internal/core/middleware/middleware.go
+++ internal/core/middleware/middleware.go
@@ -24,6 +24,7 @@ func CORS(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
